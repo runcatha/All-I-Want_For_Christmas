c2ea8ce69ebbdf9ec3a5bdfad045545fb49af6ed
Cathleen Runde <cathleenrunde@Cathleens-MBP.home>
2021-10-05 10:27:44 -0700

    data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,21 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Gift.destroy_all
+Group.destroy_all
+User.destroy_all
+
+@admin = User.create!(first_name: 'Cathleen' last_name: 'Runde' username: 'runcatha', email: 'cathleen@gmail.com', password: "password" image: "thhesdbgk")
+
+puts "#{User.count} users created"
+
+  Gift.create!(name: 'Cracking the Coding Interview', image: 'https://i.imgur.com/6x76e17.jpg', price: 24.49, buy_link: 'https://www.amazon.com/Cracking-Coding-Interview-Programming-Questions/dp/0984782850/ref=sr_1_1?dchild=1&gclid=CjwKCAjw7--KBhAMEiwAxfpkWLWtnE_YUcA4YdlNr7fyXC9fujZPzLLq0qUTYRQ892erdpcjcMeF_RoCrKQQAvD_BwE&hvadid=241870593966&hvdev=c&hvlocphy=9031520&hvnetw=g&hvqmt=e&hvrand=14727432204780906609&hvtargid=kwd-20040243067&hydadcr=16409_10304044&keywords=cracking+the+coding+interview&qid=1633452610&sr=8-1', user: @admin)
+end 
+
+@gifts = Gift.all
+
+puts "#{Gift.count} gifts created"
+
+@krachas = Group.create!(name: 'Krachas!', groups: @groups)
+
+puts ="#{Group.count} groups created!"
