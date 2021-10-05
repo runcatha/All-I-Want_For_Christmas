3fb444689f8352186c622da9b27a7bb92e26ac56
Cathleen Runde <cathleenrunde@Cathleens-MBP.home>
2021-10-05 11:00:44 -0700

    seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,17 +9,19 @@ Gift.destroy_all
 Group.destroy_all
 User.destroy_all
 
-@admin = User.create!(first_name: 'Cathleen' last_name: 'Runde' username: 'runcatha', email: 'cathleen@gmail.com', password: "password" image: "thhesdbgk")
+@admin = User.create!(first_name: 'Cathleen', last_name: 'Runde', username: 'runcatha', email: 'cathleen@gmail.com', password: "password", image: "thhesdbgk")
 
 puts "#{User.count} users created"
 
   Gift.create!(name: 'Cracking the Coding Interview', image: 'https://i.imgur.com/6x76e17.jpg', price: 24.49, buy_link: 'https://www.amazon.com/Cracking-Coding-Interview-Programming-Questions/dp/0984782850/ref=sr_1_1?dchild=1&gclid=CjwKCAjw7--KBhAMEiwAxfpkWLWtnE_YUcA4YdlNr7fyXC9fujZPzLLq0qUTYRQ892erdpcjcMeF_RoCrKQQAvD_BwE&hvadid=241870593966&hvdev=c&hvlocphy=9031520&hvnetw=g&hvqmt=e&hvrand=14727432204780906609&hvtargid=kwd-20040243067&hydadcr=16409_10304044&keywords=cracking+the+coding+interview&qid=1633452610&sr=8-1', user: @admin)
-end 
+ 
 
 @gifts = Gift.all
 
 puts "#{Gift.count} gifts created"
 
-@krachas = Group.create!(name: 'Krachas!', groups: @groups)
+@krachas = Group.create!(name: 'Krachas!', image: 'dbfjdn', members: 'Cathleen')
 
-puts ="#{Group.count} groups created!"
+puts "#{Group.count} groups created!"
+
+@admin.groups.push(@krachas)
