40038549c7f0381dc92fdb7b84a05d4a850f5030
Cathleen Runde <cathleenrunde@Cathleens-MBP.home>
2021-10-05 16:47:45 -0700

    footer

---
+++ client/src/components/Footer/Footer.jsx
@@ -0,0 +1,22 @@
+import './Footer.css'
+
+const Footer = () =>
+  <footer>
+    <div className='github-linkedIn'>
+      <a
+            className="github"
+        href='https://github.com/runcatha/All-I-Want_For_Christmas'>
+        <img
+          src='https://i.imgur.com/RshgsOc.png'
+          alt='Github'/>
+      </a>
+      <a  className="linkedIn"
+          href='https://www.linkedin.com/in/cathleen-runde-10692b204/'>
+        <img
+          src='https://i.imgur.com/9yybFiy.png'
+          alt='LinkedIn'/>
+      </a>
+    </div>
+    </footer>
+
+export default Footer
