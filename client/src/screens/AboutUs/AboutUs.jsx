50c040daf65f97d6621804ac0b642ae23b2528c2
Cathleen Runde <cathleenrunde@Cathleens-MBP.home>
2021-10-07 20:34:59 -0700

    about us css

--- client/src/screens/AboutUs/AboutUs.jsx
+++ client/src/screens/AboutUs/AboutUs.jsx
@@ -7,9 +7,9 @@ export default function AboutUs(props) {
     <div className='about-us'>
         <h1 id='about-us-hone'>About Us</h1>
       <div className='about-us-paragraph'>
-        <p>Wouldn't it be nice if you could get your friends and family exactly what they want for Christmas? 
+        <p>Wouldn't it be nice if you could get your friends and family exactly what they want for Christmas? <br /> <br /> 
 
-Also, isn't it the worst that throughout the year you think of things that would be nice to recieve as gifts, but when someone askes you what you want you draw a blank? 
+Also, isn't it the worst that throughout the year you think of things that would be nice to recieve as gifts, but when someone askes you what you want you draw a blank?  <br /> <br /> 
 
 Have no fear because All I Want for Christmas is here to solve these problems and more! Create a profile and add items that you want as they come to mind. When the holidays roll around go to your friends/families member's profiles to pick out exactly what they want.</p>
       </div>
